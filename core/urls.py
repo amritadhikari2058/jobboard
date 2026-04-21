57ea49f2bccb9c88aa0744aa5af9ac6c02291a05
Amrit Adhikari <sapsupport3@deuralijanta.com>
2026-04-21 09:23:48 +0545

    updating to new version

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["url", "username", "email", "is_staff"]
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('jobboard/', include('jobboard.urls')),
-    path('jobboard/applications/', include('applications.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("jobboard/", include("jobboard.urls")),
+    path("jobboard/applications/", include("applications.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
