a2ffd202975449786882ce12facb69ccb7980a35
amritadhikari2058 <amritadhikari2058@gmail.com>
2026-04-22 08:00:47 +0545

    Working on categories + filtering

--- jobboard/admin.py
+++ jobboard/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Job, UserRole, SavedJob, Notification
+from .models import Job, UserRole, SavedJob, Notification, Category
 
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(UserRole)
 admin.site.register(SavedJob)
 admin.site.register(Notification)
+admin.site.register(Category)
--- jobboard/models.py
+++ jobboard/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Job(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, null=False)
     slug = models.SlugField(unique=True, null=True, blank=True)
-    description = models.TextField(null=False)
     location = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=False)
+    categories = models.ManyToManyField(Category, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -25,7 +34,7 @@ class Job(models.Model):
 
 class UserRole(models.Model):
     CHOICES = [("normal_user", "Normal User"), ("recruiter", "Recruiter")]
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userrole')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="userrole")
     role = models.CharField(choices=CHOICES, default="normal_user", max_length=25)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -33,30 +42,36 @@ class UserRole(models.Model):
     def __str__(self):
         return f"{self.user}'s role"
 
+
 class SavedJob(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'job')
-    
+        unique_together = ("user", "job")
+
     def __str__(self):
-        return f'{self.user} saved job {self.job}'
-    
+        return f"{self.user} saved job {self.job}"
+
+
 class Notification(models.Model):
     STATUS_CHOICES = [
-        ('application_created', 'Application Created'),
-        ('application_accepted', 'Application Accepted'),
-        ('application_rejected', 'Application Rejected')
+        ("application_created", "Application Created"),
+        ("application_accepted", "Application Accepted"),
+        ("application_rejected", "Application Rejected"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE, null=True, blank=True)
-    application = models.ForeignKey('applications.Application', on_delete=models.CASCADE, null=True, blank=True)
+    application = models.ForeignKey(
+        "applications.Application", on_delete=models.CASCADE, null=True, blank=True
+    )
     message = models.TextField()
-    type = models.CharField(choices=STATUS_CHOICES, default='application_created', max_length=30)
+    type = models.CharField(
+        choices=STATUS_CHOICES, default="application_created", max_length=30
+    )
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user} - {self.message}'
+        return f"{self.user} - {self.message}"
