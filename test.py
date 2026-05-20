50b01f84e052a6b2e1765113304d0d09b0ddb370
Amrit Adhikari <sapsupport3@deuralijanta.com>
2026-05-20 09:26:02 +0545

    for pulling

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from jobs.models import Job
+from django.shortcuts import redirect
+from users.services import can_apply_to_job
 
 def test(request):
     jobs = Job.objects.filter(user=request.user)
@@ -10,4 +12,10 @@ def test(request):
         print(category.name)
     
     applications = job.applications.all()
-    
+
+
+def apply_to_job(request, job_id):
+    job = Job.objects.get(id=job_id)
+
+    if not can_apply_to_job(request.user, job):
+        return redirect('job_detail', id=job_id)
