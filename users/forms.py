2898c44650572b4e3597f0bcbd59bb096df7f615
Amrit Adhikari <sapsupport3@deuralijanta.com>
2026-05-24 17:48:31 +0545

    styling login and register pages

--- users/forms.py
+++ users/forms.py
@@ -16,20 +16,64 @@ class UserProfileForm(forms.ModelForm):
             "location": forms.TextInput(attrs={"class": "form-control"}),
         }
 
+
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'role']
+        fields = ["email", "role"]
     
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2 and password1!=password2:
+            raise forms.ValidationError('Passwords do not match')
+        
+        return cleaned_data
+
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Enter Email'
+        })
+
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Enter Password'
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Confirm Password'
+        })
+
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Enter your email'
+        })
+
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Enter your password'
+        })
