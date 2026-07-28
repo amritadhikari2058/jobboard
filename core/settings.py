4f60c688726da8a4b26a3fbbe789267ae79874c7
Amrit Adhikari <sapsupport3@deuralijanta.com>
2026-07-28 13:31:25 +0545

    Add PostgreSQL driver

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,9 @@ STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
 
 if DEBUG:
@@ -178,7 +181,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
